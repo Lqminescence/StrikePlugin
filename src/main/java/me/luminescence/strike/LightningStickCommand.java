bbbe468e0ac4cc30a923b58c8c3ec2701c45690b
BuildTools <unconfigured@null.spigotmc.org>
2020-08-22 15:51:57 -0400

    V 1.3 - Lightning Stick is fully functioning!

--- src/main/java/me/luminescence/strike/LightningStickCommand.java
+++ src/main/java/me/luminescence/strike/LightningStickCommand.java
@@ -4,28 +4,76 @@ package me.luminescence.strike;
 import me.luminescence.strike.utils.CC;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Objects;
 
 public class LightningStickCommand implements CommandExecutor, Listener {
 
     @EventHandler
-    public void onEvent (PlayerInteractEvent e) {
+    public void onLightningStickInteract (PlayerInteractEvent e) {
 
         Player player = e.getPlayer();
+        player.getLocation();
+
+        int set = 0;
 
         if (player.getInventory().getItemInMainHand().hasItemMeta()) {
-            if (player.getInventory().getItemInMainHand().getItemMeta().)
+            if (Objects.requireNonNull(player.getInventory().getItemInMainHand().getItemMeta()).getDisplayName().equals(ChatColor.YELLOW + "Lightning Stick")) {
 
-            player.sendMessage("You have done it!");
+                while (set < 20) {
+
+                    int xmin = -20;
+                    int xmax = 20;
+
+                    System.out.println("Random value in double from "+xmin+" to "+xmax+ ":");
+                    double doublex = Math.random() * (xmax - xmin + 1) + xmin;
+                    System.out.println(doublex);
+
+                    int zmin = -20;
+                    int zmax = 20;
+
+                    System.out.println("Random value in double from "+zmin+" to "+zmax+ ":");
+                    double doublez = Math.random() * (zmax - zmin + 1) + zmin;
+                    System.out.println(doublez);
+
+                    int finalx = (int) doublex;
+                    int finalz = (int) doublez;
+
+                    int numx = (int) player.getLocation().getX();
+
+                    int numz = (int) player.getLocation().getZ();
+
+                    double numx2 = numx + finalx;
+                    double numz2 = numz + finalz;
+
+                    System.out.println(numx2);
+                    System.out.println(numz2);
+
+                    Location location1 = new Location(player.getWorld(), numx2, 60, numz2);
+
+                    player.getWorld().strikeLightningEffect(location1);
+                    player.getWorld().strikeLightningEffect(location1);
+
+                    player.sendMessage(numx2 + ", 60, " + numz2);
+
+
+                    set++;
+                }
+
+            }
 
         } else {
 
@@ -39,7 +87,9 @@ public class LightningStickCommand implements CommandExecutor, Listener {
         ItemStack stick = new ItemStack(Material.STICK);
         ItemMeta meta = stick.getItemMeta();
         assert meta != null;
-        meta.setDisplayName("Lightning Stick");
+        meta.setDisplayName(ChatColor.YELLOW + "Lightning Stick");
+        stick.addUnsafeEnchantment(Enchantment.MENDING, 1);
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         stick.setItemMeta(meta);
 
         if (cmd.getName().equalsIgnoreCase("lstick")) {
